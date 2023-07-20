c27d813c6adf07da88d52651058b1dd730774aa6
Seulbi-Lee-project <ifssfws@gmail.com>
2023-07-20 09:44:39 +0900

    Lv1 : 더하기, 빼기, 나누기, 곱하기 연산을 수행할 수 있는 Calculator 클래스를 만들고, 클래스를 이용하여 연산을 진행하고 출력하기

---
+++ app/src/main/java/com/example/testcalculator/Main.kt
@@ -0,0 +1,29 @@
+package com.example.testcalculator
+
+fun main() {
+    var cal = Calculator()
+
+    println("숫자를 입력하세요.")
+    var num1 = readLine()!!
+    println("사칙연산을 선택해 주세요. [+] 더하기 [-] 빼기 [*] 곱하기 [/] 나누기 [%]나머지")
+    var operator = readLine()!!
+    println("숫자를 입력해 주세요.")
+    var num2 = readLine()!!
+
+    cal.operation(num1.toInt(), num2.toInt(), operator)
+
+
+}
+
+class Calculator {
+
+    fun operation (num1: Int, num2:Int, operator:String){
+        when(operator) {
+            "+" -> println("${num1} ${operator} ${num2}는 ${num1 + num2}입니다.")
+            "-" -> println("${num1} ${operator} ${num2}는 ${num1 - num2}입니다.")
+            "*" -> println("${num1} ${operator} ${num2}는 ${num1 * num2}입니다.")
+            "/" -> println("${num1} ${operator} ${num2}는 ${num1 / num2}입니다.")
+            else -> println("잘못된 계산식입니다.")
+        }
+    }
+}
